cfd55215650ae4c24132e311da235b9c439fcc80
Gautam Kumar <gautam@eccouncil.org>
2021-11-02 11:48:51 +0530

    updated php file

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -12,11 +12,11 @@ class Home extends CI_Controller
     /**
      * This is default constructor of the class
      */
-    public function __construct()
-    {
-        parent::__construct();
-        //$this->load->model('login_model');
-    }
+    // public function __construct()
+    // {
+    //     parent::__construct();
+    //     //$this->load->model('login_model');
+    // }
 
     /**
      * Index Page for this controller.

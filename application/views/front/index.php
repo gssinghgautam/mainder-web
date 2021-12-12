2270d2e6cb88b1a515972f810f2280fd2b7c257a
Aditya Bhardwaj <aditya@ohphish.com>
2021-12-12 18:15:07 +0530

    Facebook

--- application/views/front/index.php
+++ application/views/front/index.php
@@ -801,9 +801,9 @@
          <!-- end content wrapping -->
       </div>
       <!-- end content section -->
-      <iframe src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2FHinduSanatanMandir&tabs=timeline&width=340&height=500&small_header=false&adapt_container_width=true&hide_cover=false&show_facepile=true&appId" width="340" height="500" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowfullscreen="true" allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share"></iframe>
+    <!--   <iframe src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2FHinduSanatanMandir&tabs=timeline&width=340&height=500&small_header=false&adapt_container_width=true&hide_cover=false&show_facepile=true&appId" width="340" height="500" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowfullscreen="true" allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share"></iframe>
 
-   </div>
+    --></div>
    <!-- end container -->
 
 </section>
@@ -815,7 +815,7 @@
       <div class="content-section">
          <div class="content-wrapper">
             <div class="map-container">
-               <!-- <div class="googleMaps google-map-container"></div> -->
+               <div class="googleMaps google-map-container"></div>
                  <iframe src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2FHinduSanatanMandir&tabs=timeline&width=340&height=500&small_header=false&adapt_container_width=true&hide_cover=false&show_facepile=true&appId" width="100%" height="500" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowfullscreen="true" allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share"></iframe>
 
                <div class="contact-info col-md-6 pull-right">

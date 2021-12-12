7b28d268d18f98d8bb2d454e11e41f9573ba51e2
Aditya Bhardwaj <aditya@ohphish.com>
2021-12-12 18:49:20 +0530

    Facebook

--- application/views/front/index.php
+++ application/views/front/index.php
@@ -816,7 +816,7 @@
             <div class="map-container">
                <!-- <div class="googleMaps google-map-container"></div> -->
 
-               <iframe src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2FHinduSanatanMandir&tabs=timeline&width=800&height=850&small_header=false&adapt_container_width=true&hide_cover=false&show_facepile=true&appId" width="800" height="850" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowfullscreen="true" allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share"></iframe>
+               <iframe src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2FHinduSanatanMandir&tabs=timeline&width=900&height=850&small_header=false&adapt_container_width=true&hide_cover=false&show_facepile=true&appId" width="900" height="850" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowfullscreen="true" allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share"></iframe>
 
 
                <div class="contact-info col-md-6 pull-right">

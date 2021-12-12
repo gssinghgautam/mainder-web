dc557d643ba0532432fe9f858e5d238dfe1429f4
Aditya Bhardwaj <aditya@ohphish.com>
2021-12-12 18:07:47 +0530

    Facebook

--- application/views/front/index.php
+++ application/views/front/index.php
@@ -803,9 +803,10 @@
       <!-- end content section -->
    </div>
    <!-- end container -->
+   <iframe src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2FHinduSanatanMandir&tabs=timeline&width=340&height=500&small_header=false&adapt_container_width=true&hide_cover=false&show_facepile=true&appId" width="340" height="500" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowfullscreen="true" allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share"></iframe>
+
 </section>
 
-<iframe src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2FHinduSanatanMandir&tabs=timeline&width=340&height=500&small_header=false&adapt_container_width=true&hide_cover=false&show_facepile=true&appId" width="340" height="500" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowfullscreen="true" allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share"></iframe>
 <!-- end contact us -->
 <!-- start full width -->
 <section class="full-width">

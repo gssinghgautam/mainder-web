4422e1ead664c106e99e68510d3f31feb7f74556
Aditya Bhardwaj <aditya@ohphish.com>
2021-12-22 16:09:12 +0530

    email

--- application/views/front/committee.php
+++ application/views/front/committee.php
@@ -18,94 +18,14 @@
                                       
                                     </div>
                                    
-                                    <div class="col-md-4 col-sm-4">
-                                       <div class="team-member">
-                                          
-                                          <div class="team-hover">
-                                             <div class="desk">
-                                                <h4>Madan Lal Gupta</h4>
-                                             </div>
-                                          </div>
-                                       </div>
-                                       <div class="team-title">
-                                          <h5>President</h5>
-                                       </div>
-                                    </div>
-
-                                    <div class="col-md-4 col-sm-4">
-                                       <div class="team-member">
-                                          
-                                          <div class="team-hover">
-                                             <div class="desk">
-                                                <h4>Abnash Gupta</h4>
-                                             </div>
-                                          </div>
-                                       </div>
-                                       <div class="team-title">
-                                          <h5>Vice President</h5>
-                                       </div>
-                                    </div>
-
-                                    <div class="col-md-4 col-sm-4">
-                                       <div class="team-member">
-                                         
-                                          <div class="team-hover">
-                                             <div class="desk">
-                                                <h4>Vijay Kumar Saggar </h4>
-                                             </div>
-                                          </div>
-                                       </div>
-                                       <div class="team-title">
-                                          <h5>Cashier</h5>
-                                       </div>
-                                    </div>
+                                    
 
                                  </div>
                                 <br><br>
-                                 <div class="row">
-                                    <div class="heading-title text-center">
-                                       <!--   <p class="p-top-30 half-txt">Nam pulvinar vitae neque et porttitor. Praesent sed nisi eleifend. Nam pulvinar vitae neque et porttitor. Praesent sed nisi eleifend. </p> -->
-                                    </div>
-                                    <div class="col-md-4 col-sm-4">
-                                       <div class="team-member">
-                                         
-                                          <div class="team-hover">
-                                             <div class="desk">
-                                                <h4>Boota Mahe</h4>
-                                             </div>
-                                          </div>
-                                       </div>
-                                       <div class="team-title">
-                                          <h5>Secretary & Kitchen Management</h5>
-                                       </div>
-                                    </div>
-                                    <div class="col-md-4 col-sm-4">
-                                       <div class="team-member">
-                                          
-                                          <div class="team-hover">
-                                             <div class="desk">
-                                                <h4>Sanjay Gupta</h4>
-                                             </div>
-                                          </div>
-                                       </div>
-                                       <div class="team-title">
-                                          <h5>General Administration</h5>
-                                       </div>
-                                    </div>
-                                    <div class="col-md-4 col-sm-4">
-                                       <div class="team-member">
-                                         
-                                          <div class="team-hover">
-                                             <div class="desk">
-                                                <h4>Sumit Monga</h4>
-                                             </div>
-                                          </div>
-                                       </div>
-                                       <div class="team-title">
-                                          <h5>Relationship Management </h5>
-                                       </div>
-                                    </div>
-                                 </div>
+                                  
+
+
+
                                  <div class="row">
                                     <div class="heading-title text-center">
                                        <!--   <p class="p-top-30 half-txt">Nam pulvinar vitae neque et porttitor. Praesent sed nisi eleifend. Nam pulvinar vitae neque et porttitor. Praesent sed nisi eleifend. </p> -->
@@ -150,6 +70,28 @@
                                        </div>
                                     </div>
                                  </div>
+
+                                 <div class="row">
+                                    <table>
+  <tr>
+    <th>Company</th>
+    <th>Contact</th>
+    <th>Country</th>
+  </tr>
+  <tr>
+    <td>Alfreds Futterkiste</td>
+    <td>Maria Anders</td>
+    <td>Germany</td>
+  </tr>
+  <tr>
+    <td>Centro comercial Moctezuma</td>
+    <td>Francisco Chang</td>
+    <td>Mexico</td>
+  </tr>
+</table>
+                                 </div>
+
+
                               </div>
                            </div>
                            <!--  end event desc-->
